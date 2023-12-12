7d35bedd01255ea456cb8a0517a564828fd30db1
irinaribeiro <40610352+irinaribeiro@users.noreply.github.com>
2023-12-12 14:12:58 +0000

    Password protected page

---
+++ js/pass_word_es.js
@@ -0,0 +1,24 @@
+/*!
+ * Bootstrap v3.3.5 (http://getbootstrap.com)
+ * Copyright 2011-2015 Twitter, Inc.
+ * Licensed under the MIT license
+ */
+
+	function passWord() {
+	var testV = 1;
+	var pass1 = prompt('Password','');
+	while (testV < 3) {
+	if (!pass1)
+	history.go(-1);
+	if (pass1.toLowerCase() == "mypassword") {
+	window.location.replace('https://helder-furniture.com/work_es.html');
+	break;
+	}
+	testV+=1;
+	var pass1 =
+	prompt('Acceso denegado: contraseña incorrecta, inténtelo de nuevo.','');
+	}
+	if (pass1.toLowerCase()!="password" & testV ==3)
+	history.go(-1);
+	return " ";
+	}
